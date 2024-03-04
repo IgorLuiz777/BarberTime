a5bd5a3c1ddac64c84e703fd7b36244d4774d6b7
IgorLuiz777 <igorluizpereiralima@gmail.com>
2024-03-04 13:19:43 -0300

    building project

---
+++ src/main/java/br/com/fiap/barbertime/BarbertimeApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.barbertime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbertimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbertimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/barbertime/BarbertimeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.barbertime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbertimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
