1fe6c24680f5b8d7dbccfaffc3ef511013adc385
IgorLuiz777 <igorluizpereiralima@gmail.com>
2024-03-06 21:26:43 -0300

    feat: adding getId and post barbearia

--- src/main/java/br/com/fiap/barbertime/controller/BarbeariaController.java
+++ src/main/java/br/com/fiap/barbertime/controller/BarbeariaController.java
@@ -3,9 +3,12 @@ package br.com.fiap.barbertime.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,10 +23,12 @@ public class BarbeariaController {
 
  
     //repository
+    //@AutoWired
     List<Barbearia> barbeariaRepository = new ArrayList<>();
 
     //post
     @PostMapping()
+    //@Transactional
     public ResponseEntity<Barbearia> cadastrarBabearia(@RequestBody Barbearia barbearia) {
         
         barbeariaRepository.add(barbearia);
@@ -31,11 +36,36 @@ public class BarbeariaController {
     }
 
     //getAll
-    @GetMapping  
+    @GetMapping()
     public List<Barbearia> listaBarbearias() {
 
         return barbeariaRepository;
     }
+
+    //getId
+    @GetMapping("/{id}")
+    public ResponseEntity<Barbearia> detalharBarbearia(@PathVariable  Long id) {
+        
+        for (Barbearia barbearia : barbeariaRepository) {
+            if (barbearia.id().equals(id)) {
+                return ResponseEntity.status( HttpStatus.OK).body(barbearia);
+            } 
+        } return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Barbearia> excluirBarbearia(@PathVariable Long id) {
+        for (Barbearia barbearia : barbeariaRepository) {
+            if (barbearia.id().equals(id)) {
+                barbeariaRepository.remove(barbearia);
+                return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+            }
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
 
 
 }
--- src/main/java/br/com/fiap/barbertime/model/Barbearia.java
+++ src/main/java/br/com/fiap/barbertime/model/Barbearia.java
@@ -2,4 +2,6 @@ package br.com.fiap.barbertime.model;
 
 public record Barbearia(Long id, String nome, Endereco endereco, String email, String telefone, String cnpj, Servicos servicos, Funcionario funcionario) {
      
+      
+
 }
