69bf9acdad97ac61b84b473064e671f2cea6d85b
IgorLuiz777 <igorluizpereiralima@gmail.com>
2024-05-13 18:13:01 -0300

    feat: adding HATEOAS Barbearia model

--- src/main/java/br/com/fiap/barbertime/controller/BarbeariaController.java
+++ src/main/java/br/com/fiap/barbertime/controller/BarbeariaController.java
@@ -1,25 +1,37 @@
 package br.com.fiap.barbertime.controller;
 
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import br.com.fiap.barbertime.model.Barbearia;
 import br.com.fiap.barbertime.repository.BarbeariaRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
-
-import static org.springframework.http.HttpStatus.*;
 
 @RestController
 @RequestMapping("/barbearia")
@@ -30,21 +42,26 @@ public class BarbeariaController {
     @Autowired
     private BarbeariaRepository barbeariaRepository;
 
+    @Autowired
+    PagedResourcesAssembler<Barbearia> pagedResourcesAssembler;
+
     @GetMapping
     @Operation(
         summary = "Listar todas as barbearias",
         description = "Retorna uma lista paginada de todas as barbearias, ordenadas pelo nome em ordem alfabética"
     )
-    public Page<Barbearia> listarBarbearias(
+    public PagedModel<EntityModel<Barbearia>> listarBarbearias(
             @Parameter(description = "Página solicitada (começando do 0)", example = "0") @PageableDefault(sort = "nome", direction = Direction.ASC) Pageable pageable) {
-        return barbeariaRepository.findAll(pageable);
+    
+        Page<Barbearia> page = barbeariaRepository.findAll(pageable);
+        return pagedResourcesAssembler.toModel(page);
     }
 
+    @GetMapping("/nome")
     @Operation(
-        summary = "Burcar uma barbearias pelo nome",
+        summary = "Buscar uma barbearia pelo nome",
         description = "Retorna uma barbearia com base no nome fornecido"
     )
-    @GetMapping("/nome")
     public Page<Barbearia> findByNome(
             @RequestParam(required = false) String nome, Pageable pageable) {
         return barbeariaRepository.findByNome(nome, pageable);
@@ -57,14 +74,16 @@ public class BarbeariaController {
     )
     @ApiResponses(value = {
         @ApiResponse(responseCode = "200", description = "Barbearia encontrada"),
-        @ApiResponse(responseCode = "404", description = "Barbearia não encontrada", content = @Content)
+        @ApiResponse(responseCode = "404", description = "Barbearia não encontrada", content = @io.swagger.v3.oas.annotations.media.Content)
     })
-    public ResponseEntity<Barbearia> buscarPorId(
+    public EntityModel<Barbearia> buscarPorId(
             @Parameter(description = "ID da barbearia a ser buscada", example = "1") @PathVariable Long id) {
         log.info("Buscando barbearia com o ID {}", id);
-        return barbeariaRepository.findById(id)
-                .map(barbearia -> ResponseEntity.ok().body(barbearia))
-                .orElse(ResponseEntity.notFound().build());
+
+        var barbearia = barbeariaRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "Barbearia não encontrada"));
+
+        return barbearia.toEntityModel();
     }
 
     @PostMapping
@@ -75,11 +94,12 @@ public class BarbeariaController {
     )
     @ApiResponses(value = {
         @ApiResponse(responseCode = "201", description = "Barbearia cadastrada com sucesso"),
-        @ApiResponse(responseCode = "400", description = "Requisição inválida. Verifique o corpo da requisição", content = @Content)
+        @ApiResponse(responseCode = "400", description = "Requisição inválida. Verifique o corpo da requisição", content = @io.swagger.v3.oas.annotations.media.Content)
     })
     public Barbearia cadastrarBarbearia(
             @Parameter(description = "Dados da barbearia a ser cadastrada") @RequestBody Barbearia barbearia) {
         log.info("Cadastrando nova barbearia: {}", barbearia);
+
         return barbeariaRepository.save(barbearia);
     }
 
@@ -90,37 +110,40 @@ public class BarbeariaController {
     )
     @ApiResponses(value = {
         @ApiResponse(responseCode = "200", description = "Barbearia atualizada com sucesso"),
-        @ApiResponse(responseCode = "404", description = "Barbearia não encontrada", content = @Content)
+        @ApiResponse(responseCode = "404", description = "Barbearia não encontrada", content = @io.swagger.v3.oas.annotations.media.Content)
     })
     public Barbearia atualizarBarbearia(
             @Parameter(description = "ID da barbearia a ser atualizada", example = "1") @PathVariable Long id,
             @Parameter(description = "Novos dados da barbearia") @RequestBody Barbearia barbearia) {
         log.info("Atualizando a barbearia com o ID {} para: {}", id, barbearia);
-        verificarExistenciaBarbearia(id);
+        
+        if (!barbeariaRepository.existsById(id)) {
+            throw new ResponseStatusException(NOT_FOUND, "Barbearia não encontrada: ID " + id);
+        }
+
         barbearia.setId(id);
         return barbeariaRepository.save(barbearia);
     }
 
     @DeleteMapping("/{id}")
-    @ResponseStatus(NO_CONTENT)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @Operation(
         summary = "Excluir uma barbearia",
         description = "Exclui uma barbearia com base no ID fornecido"
     )
     @ApiResponses(value = {
         @ApiResponse(responseCode = "204", description = "Barbearia excluída com sucesso"),
-        @ApiResponse(responseCode = "404", description = "Barbearia não encontrada", content = @Content)
+        @ApiResponse(responseCode = "404", description = "Barbearia não encontrada", content = @io.swagger.v3.oas.annotations.media.Content)
     })
     public void excluirBarbearia(
             @Parameter(description = "ID da barbearia a ser excluída", example = "1") @PathVariable Long id) {
         log.info("Excluindo a barbearia com o ID {}", id);
-        verificarExistenciaBarbearia(id);
-        barbeariaRepository.deleteById(id);
-    }
 
-    private void verificarExistenciaBarbearia(Long id) {
         if (!barbeariaRepository.existsById(id)) {
             throw new ResponseStatusException(NOT_FOUND, "Barbearia não encontrada com o ID: " + id);
         }
+
+        barbeariaRepository.deleteById(id);
     }
+
 }
--- src/main/java/br/com/fiap/barbertime/model/Barbearia.java
+++ src/main/java/br/com/fiap/barbertime/model/Barbearia.java
@@ -1,8 +1,13 @@
 package br.com.fiap.barbertime.model;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 import java.util.List;
 
+import org.springframework.hateoas.EntityModel;
+
+import br.com.fiap.barbertime.controller.BarbeariaController;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,6 +19,7 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
@@ -21,7 +27,8 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Barbearia {
+@EqualsAndHashCode(callSuper = false)
+public class Barbearia extends EntityModel<Barbearia>{
 
     // TODO: CONSEGUIR DA UM PUT SEM PASSAR OS ATRIBULOS NOTNULL
      
@@ -46,5 +53,16 @@ public class Barbearia {
     List<Servicos> servicos;
 
     //Funcionario funcionario;
+
+     public EntityModel<Barbearia> toEntityModel() {
+        return EntityModel.of(
+            this, 
+            linkTo(methodOn(BarbeariaController.class).buscarPorId(id)).withSelfRel(),
+            linkTo(BarbeariaController.class).slash(id).withRel("DELETE"),
+            linkTo(methodOn(BarbeariaController.class).listarBarbearias(null)).withRel("GET"),
+            linkTo(methodOn(BarbeariaController.class).cadastrarBarbearia(null)).withRel("POST"),
+            linkTo(methodOn(BarbeariaController.class).atualizarBarbearia(id, null)).withRel("PUT")
+            );
+    }
 
 }
