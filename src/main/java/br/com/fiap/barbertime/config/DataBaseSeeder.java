bb6735239ddaa430c08db46904474a82e4e40cd3
IgorLuiz777 <igorluizpereiralima@gmail.com>
2024-05-16 11:41:44 -0300

    feat: adding oracle DataBase and deploy

--- src/main/java/br/com/fiap/barbertime/config/DataBaseSeeder.java
+++ src/main/java/br/com/fiap/barbertime/config/DataBaseSeeder.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import br.com.fiap.barbertime.model.Barbearia;
 import br.com.fiap.barbertime.model.Servicos;
@@ -13,6 +14,7 @@ import br.com.fiap.barbertime.repository.BarbeariaRepository;
 import br.com.fiap.barbertime.repository.ServicosRepository;
 
 @Configuration
+@Profile("dev")
 public class DataBaseSeeder implements CommandLineRunner {
 
     @Autowired
