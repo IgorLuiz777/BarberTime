ab86a2bec516762cfdf549e6a998586f405d5f60
IgorLuiz777 <igorluizpereiralima@gmail.com>
2024-05-25 13:15:14 -0300

    feat: adding and implementing User repository and service

--- src/main/java/br/com/fiap/barbertime/model/User.java
+++ src/main/java/br/com/fiap/barbertime/model/User.java
@@ -1,10 +1,13 @@
 package br.com.fiap.barbertime.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @NoArgsConstructor
@@ -16,11 +19,16 @@ public class User {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     private String nomeUsuario;
 
+    @NotBlank
     private String senha;
 
     @Column(unique = true)
     private String email;
+
+    @ManyToMany
+    private List<Role> roles;
 
 }
---
+++ src/main/java/br/com/fiap/barbertime/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package br.com.fiap.barbertime.repository;
+
+import br.com.fiap.barbertime.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+}
